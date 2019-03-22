272e19dc0df810b589c1fe5e3ed31ab38db6fc4d
Furkan Yavuz <furknyavuz@gmail.com>
2019-03-23 01:09:14 +0300

    #

--- src/templates/all-releases.js
+++ src/templates/all-releases.js
@@ -9,6 +9,7 @@ export default ({ pageContext: { releases } }) => (
     <div style={{ width: 960, margin: "4rem auto" }}>
       <h2>Track latest releases of popular github repositories.</h2>
       {releases.map(release => (
+        release.tagName &&
         <a href={`https://www.github.com${release.resourcePath}`} target="_blank">
           <p>{`${release.owner}/${release.name} latest release: ${release.tagName}`}</p>
         </a>
