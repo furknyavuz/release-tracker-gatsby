07d09fdaa6b2cc975a23549b6e690ea9e274b8f7
Furkan Yavuz <furknyavuz@gmail.com>
2019-03-23 23:49:50 +0300

    #

--- src/components/header.js
+++ src/components/header.js
@@ -9,13 +9,7 @@ const Header = ({ siteTitle }) => (
       marginBottom: `1.45rem`,
     }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+    <div className="headerDiv">
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
