69db50253065a5026e5248916f169f23c3ae1225
Furkan Yavuz <furknyavuz@gmail.com>
2019-03-24 02:19:32 +0300

    #

--- src/components/seo.js
+++ src/components/seo.js
@@ -77,7 +77,15 @@ function SEO({ description, lang, meta, keywords, title }) {
             : []
         )
         .concat(meta)}
-    />
+    >
+      {/* Global site tag (gtag.js) - Google Analytics  */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136224529-2">
+
+      </script>
+      <script>
+        {"window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-136224529-2');"}
+      </script>
+    </Helmet>
   )
 }
 
