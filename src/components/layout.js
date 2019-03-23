81bb6bee1951588200029988c00f601fa568c57f
Furkan Yavuz <furknyavuz@gmail.com>
2019-03-23 18:12:49 +0300

    #

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ import { StaticQuery, graphql } from "gatsby";
 
 import Header from "./header";
 import "./layout.css";
-import { Container } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Layout = ({ children }) => (
@@ -29,18 +29,11 @@ const Layout = ({ children }) => (
       <>
         <Header siteTitle={data.site.siteMetadata.title}/>
         <Container>
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `0px 1.0875rem 1.45rem`,
-              paddingTop: 0
-            }}
-          >
-            <main>{children}</main>
-            <footer>
-            </footer>
-          </div>
+          <Row>
+            <Col>
+              <main>{children}</main>
+            </Col>
+          </Row>
         </Container>
       </>
     )}
--- src/templates/all-releases.js
+++ src/templates/all-releases.js
@@ -2,26 +2,82 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import { Button, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from "reactstrap";
+import {
+  Button,
+  ListGroup,
+  ListGroupItem,
+  ListGroupItemHeading,
+  ListGroupItemText,
+  Container, Row, Col
+} from "reactstrap";
 
 export default ({ pageContext: { releases } }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`, `github`]}/>
-    <div style={{ width: 960, margin: "4rem auto" }}>
-      <h3>Track latest releases of popular github repositories</h3>
-      <ListGroup>
-        {releases.map(release => (
-          release.tagName &&
-          <ListGroupItem>
-            <ListGroupItemHeading>{`${release.owner}/${release.name}`}</ListGroupItemHeading>
-            <ListGroupItemText>
-              <a href={`https://www.github.com${release.resourcePath}`} rel="noopener noreferrer" target="_blank">
-                <Button size="sm" outline color="success">{`Latest release: ${release.tagName}`}</Button>
-              </a>
-            </ListGroupItemText>
-          </ListGroupItem>
-        ))}
-      </ListGroup>
-    </div>
+    <h3 style={{ padding: "2px", paddingBottom: "10px" }}>Latest releases of popular repositories</h3>
+    <ListGroup>
+      {releases.map(release => (
+        release.tagName &&
+        <ListGroupItem>
+          <ListGroupItemHeading className="text-muted">
+            {`${release.owner}/${release.name}`}
+          </ListGroupItemHeading>
+          <hr/>
+          <ListGroupItemText>
+            <Container fluid={true}>
+              <Row>
+                <Col style={{ padding: "2px" }}>
+                  <Container fluid={true}>
+                    <Row style={{ padding: "4px", paddingBottom: "15px", paddingTop: "10px" }}>
+                      {`${release.description}`}
+                    </Row>
+                    <Row style={{ paddingBottom: "10px"}}>
+                      <a style={{ padding: "2px", paddingLeft: "5px", paddingRight: "5px" }} href={`https://www.github.com${release.resourcePath}`}
+                         rel="noopener noreferrer" target="_blank">
+                        <Button size="sm" outline color="success">{`Latest release: ${release.tagName}`}</Button>
+                      </a>
+                      <a style={{ padding: "2px", paddingLeft: "5px", paddingRight: "5px" }} href={`${release.homepageUrl}`} rel="noopener noreferrer"
+                         target="_blank">
+                        <Button size="sm" outline color="secondary">{`${release.homepageUrl}`}</Button>
+                      </a>
+                    </Row>
+                  </Container>
+                </Col>
+                <Col md={3} style={{ padding: "2px" }}>
+                  <Container fluid={true}>
+                    <Row>
+                      <Col>
+                        <iframe
+                          src={`https://ghbtns.com/github-btn.html?user=${release.owner}&repo=${release.name}&type=star&count=true&size=large`}
+                          frameBorder="0" scrolling="0" width="170px" height="30px">`
+                        </iframe>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <iframe
+                          src={`https://ghbtns.com/github-btn.html?user=${release.owner}&repo=${release.name}&type=watch&count=true&v=2&size=large`}
+                          frameBorder="0" scrolling="0" width="170px" height="30px">
+
+                        </iframe>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <iframe
+                          src={`https://ghbtns.com/github-btn.html?user=${release.owner}&repo=${release.name}&type=fork&count=true&size=large`}
+                          frameBorder="0" scrolling="0" width="170px" height="30px">
+
+                        </iframe>
+                      </Col>
+                    </Row>
+                  </Container>
+                </Col>
+              </Row>
+            </Container>
+          </ListGroupItemText>
+        </ListGroupItem>
+      ))}
+    </ListGroup>
   </Layout>
 )
